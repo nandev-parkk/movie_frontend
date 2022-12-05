53ffdc46924d44dec3245dbea48b1e76f1fc976b
nandev <nandev.parkk@gmail.com>
2022-12-05 23:28:02 +0900

    test

--- modules/Http.js
+++ modules/Http.js
@@ -3,13 +3,21 @@ import axios from "axios";
 
 class Http {
   constructor() {
-    const instance = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_NAVER_BASE_URL,
-      headers: {
-        "X-Naver-Client-Id": process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-        "X-Naver-Client-Secret": process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET,
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_NAVER_BASE_URL;
+    axios.defaults.headers.common["X-Naver-Client-Id"] =
+      process.env.NEXT_PUBLIC_NAVER_CLIENT_ID;
+    axios.defaults.headers.common["X-Naver-Client-Secret"] =
+      process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET;
+  }
+
+  async getMovies(query) {
+    const res = await axios.get("/", {
+      params: {
+        query,
       },
     });
+
+    return res;
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div></div>;
+  return <h1>찬빈아 안녕!</h1>;
 }
